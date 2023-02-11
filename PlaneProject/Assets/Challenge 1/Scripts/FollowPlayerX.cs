0ea462d048fca70d61839303c9c5ce8cd120e52c
Elmar055 <104317952+Elmar055@users.noreply.github.com>
2023-02-11 22:30:48 +0400

    All files upload

---
+++ PlaneProject/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane; // the player's object
+    private Vector3 offset = new Vector3(17.59f,1.47f,1.43f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move the camera away from the player
+        transform.position = plane.transform.position + offset; 
+    }
+}
